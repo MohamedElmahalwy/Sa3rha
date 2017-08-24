e08428793eb0809fc6d276cc16dffaab53b823b9
MagedEldamasy <maged.eldamasy@gmail.com>
2017-08-24 08:41:05 -0700

    Maged Modify last activity (#3)
    * add last activity ,add some design points in other activities
    
    * modify last activity ,add some design points in other activities

--- app/src/main/java/com/sa3rha/android/sa3rha/Ui/Fragments/Compare_FristFragment.java
+++ app/src/main/java/com/sa3rha/android/sa3rha/Ui/Fragments/Compare_FristFragment.java
@@ -1,11 +1,16 @@
 package com.sa3rha.android.sa3rha.Ui.Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 import com.sa3rha.android.sa3rha.R;
 
 
@@ -13,13 +18,97 @@ import com.sa3rha.android.sa3rha.R;
  * Created by Maged on 8/24/2017.
  */
 
-public class Compare_FristFragment extends Fragment{
+public class Compare_FristFragment extends Fragment implements YouTubePlayer.OnInitializedListener {
+    static final String Api_Key="AIzaSyBllWxxH8RoWCElkiTBP1XZOQZ8hdEY3As";
+    static final String VedioID="1N9KveJ-FU8";
+    YouTubePlayerSupportFragment youTubePlayerFragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_compare,container,false);
+        youTubePlayerFragment = (YouTubePlayerSupportFragment) getActivity().getSupportFragmentManager()
+                .findFragmentById(R.id.youtube_fragment);
         return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+         youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
+         youTubePlayerFragment.initialize(VedioID,this);
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.youtube_fragment, youTubePlayerFragment).commit();
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, final YouTubePlayer youTubePlayer, boolean b) {
+        if(!b){
+            youTubePlayer.cueVideo(VedioID);
+        }
+        YouTubePlayer.PlayerStateChangeListener playerStateChangeListener= new YouTubePlayer.PlayerStateChangeListener() {
+            @Override
+            public void onLoading() {
+
+            }
+
+            @Override
+            public void onLoaded(String s) {
+                youTubePlayer.pause();
+            }
+
+            @Override
+            public void onAdStarted() {
+                youTubePlayer.next();
+            }
+
+            @Override
+            public void onVideoStarted() {
+
+            }
+
+            @Override
+            public void onVideoEnded() {
+            }
+
+            @Override
+            public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+            }
+        };
+        YouTubePlayer.PlaybackEventListener playbackEventListener=new YouTubePlayer.PlaybackEventListener() {
+            @Override
+            public void onPlaying() {
+
+            }
+
+            @Override
+            public void onPaused() {
+
+            }
+
+            @Override
+            public void onStopped() {
+
+            }
+
+            @Override
+            public void onBuffering(boolean b) {
+
+            }
+
+            @Override
+            public void onSeekTo(int i) {
+
+            }
+        };
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
 }
